6c110c4b74b134bb3cc7df49d2df3d0777c11482
BKopysc <bkopysc@wp.pl>
2021-12-07 14:13:11 +0100

    guzik do zamykania widgetów - nie działa jak na razie

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,13 +1,29 @@
 import WidgetFrame from "../../elements/WidgetFrame/WidgetFrame";
 import styles from "./dashboard.module.css"
-function Dashboard(){
-    return(
-      <div className={styles.dashboardDiv}>
-        <WidgetFrame />
-        <WidgetFrame />
-        <WidgetFrame />
-      </div>
-    );
+function Dashboard() {
+
+  // let WidgetArray = [
+  //   { id: 1, widget: <WidgetFrame OnCloseButton={OnCloseButton} /> },
+  //   { id: 2, widget: <WidgetFrame OnCloseButton={OnCloseButton} /> },
+  //   { id: 3, widget: <WidgetFrame OnCloseButton={OnCloseButton} /> }
+  // ]
+  // const OnCloseButton = (widgetFrame) => {
+  //   widgetFrame.style=styles.hiddenFrame
+  // }
+    const OnCloseButton = () => {
+    console.log("close widget");
+  }
+
+  return (
+    <div className={styles.dashboardDiv}>
+      <WidgetFrame
+        OnCloseButton={OnCloseButton} />
+      <WidgetFrame
+        OnCloseButton={OnCloseButton} />
+      <WidgetFrame
+        OnCloseButton={OnCloseButton} />
+    </div>
+  );
 }
 
 export default Dashboard;
--- src/elements/WidgetFrame/WidgetFrame.js
+++ src/elements/WidgetFrame/WidgetFrame.js
@@ -1,9 +1,15 @@
 import styles from "./widgetFrame.module.css"
-
-export default function WidgetFrame(){
-    return(
-        <div className={styles.widgetDiv}>
-            <text>PlaceForWidget</text>           
+import CloseButton from "../../assets/icons/close-icon.png"
+export default function WidgetFrame(props) {
+    return (
+        <div className={styles.widgetFrame}>
+            <label className={styles.labelFrame}>LabelName</label>
+            <div className={styles.widgetDiv}>
+                <button className={styles.closeButton} onClick={props.OnCloseButton}>
+                    <img src={CloseButton} className={styles.imgIcon} />
+                </button>
+                <text>PlaceForWidget</text>
+            </div>
         </div>
     );
 }
