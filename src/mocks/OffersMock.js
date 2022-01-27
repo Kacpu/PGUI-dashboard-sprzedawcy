d936f51d1b7bfd867be49409f3d1706ce487d56c
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-01-27 11:23:41 +0100

    mocks for offers

--- src/mocks/OffersMock.js
+++ src/mocks/OffersMock.js
@@ -1,37 +1,37 @@
 export const offers = [
     {
         id: 0,
-        img : '',
-        name: "",
-        pcs: 1,
-        earning: 1
+        img : '../../mocks/mocks-img/hat.jpg',
+        name: "Kapelusz strażaka",
+        pcs: 60,
+        earning: 6000
     },
     {
         id: 1,
-        img : '',
-        name: "",
-        pcs: 1,
-        earning: 1
+        img : '../../mocks/mocks-img/glasses.jpg',
+        name: "Okulary czarodzieja",
+        pcs: 100,
+        earning: 5000
     },
     {
         id: 2,
-        img : '',
-        name: "",
-        pcs: 1,
-        earning: 1
+        img : '../../mocks/mocks-img/shoe.jpg',
+        name: "But Kopciuszka",
+        pcs: 40,
+        earning: 8000
     },
     {
         id: 3,
-        img : '',
-        name: "",
-        pcs: 1,
-        earning: 1
+        img : '../../mocks/mocks-img/shirt.jpg',
+        name: "Koszulka mozilla",
+        pcs: 20,
+        earning: 2000
     },
     {
         id: 4,
-        img : '',
-        name: "",
-        pcs: 1,
-        earning: 1
+        img : '../../mocks/mocks-img/socks.jpg',
+        name: "Skarpetki biedronki",
+        pcs: 10,
+        earning: 1000
     }
 ]
